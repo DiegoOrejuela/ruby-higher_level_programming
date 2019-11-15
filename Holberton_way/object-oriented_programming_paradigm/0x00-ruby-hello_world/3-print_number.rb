00388d7f353563cadc43b667f8c7874934e166cc
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-11-15 20:39:42 +0000

    [feature] print the integer stored in the variable number, followed by Battery street, followed by a new line

---
+++ Holberton_way/object-oriented_programming_paradigm/0x00-ruby-hello_world/3-print_number.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+#Complete this source code in order to print the integer stored in the variable
+#number, followed by Battery street, followed by a new line.
+number = 98
+puts "#{number} Battery street"
