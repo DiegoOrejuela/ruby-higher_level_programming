ee6cd91408da02b6f7fe44bce6852bef07e50aba
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-11-15 20:26:39 +0000

    [feature] script that prints exactly 'Programming is like building a multilingual puzzle, followed by a new line.'

---
+++ Holberton_way/object-oriented_programming_paradigm/0x00-ruby-hello_world/2-print.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/ruby
+#Write a Ruby script that prints exactly "Programming is like building
+#a multilingual puzzle, followed by a new line.
+puts "\"Programming is like building a multilingual puzzle"
