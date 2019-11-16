4bbcb3edce1a8578a1800679ab1f9fcf1961df82
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-11-16 15:47:50 +0000

    [FEATURE] script that print description on Ruby

---
+++ Holberton_way/object-oriented_programming_paradigm/0x00-ruby-hello_world/9-easter_egg.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+#Write a Python script that prints “The Zen of ruby”
+str = "You can use Ruby to write servers, experiment with prototypes, and for\
+everyday programming tasks. As a fully-integrated object-oriented language, Ruby scales well."
+puts str
